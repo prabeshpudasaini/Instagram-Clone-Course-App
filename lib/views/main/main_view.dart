91ee9129c654e8ebb6dd10ae60973a9046a22c53
pudasainiprabesh56 <pudasainiprabesh56@gmail.com>
2024-08-26 15:59:56 +0000

    Implemented image upload

--- lib/views/main/main_view.dart
+++ lib/views/main/main_view.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instagram_clone_app/state/auth/providers/auth_state_provider.dart';
+import 'package:instagram_clone_app/state/image_upload/helpers/image_picker_helper.dart';
+import 'package:instagram_clone_app/state/image_upload/models/file_type.dart';
+import 'package:instagram_clone_app/state/post_settings/providers/post_settings_provider.dart';
 import 'package:instagram_clone_app/views/components/dialogs/alert_dialog_model.dart';
 import 'package:instagram_clone_app/views/components/dialogs/logout_dialog.dart';
 import 'package:instagram_clone_app/views/constants/strings.dart';
+import 'package:instagram_clone_app/views/create_new_post/create_new_post_view.dart';
 import 'package:instagram_clone_app/views/tabs/user_posts/user_posts_view.dart';
 
 class MainView extends ConsumerStatefulWidget {
@@ -27,13 +31,51 @@ class _MainViewState extends ConsumerState<MainView> {
           ),
           actions: [
             IconButton(
-              onPressed: () async {},
+              onPressed: () async {
+                final videoFile =
+                    await ImagePickerHelper.pickVideoFromGallery();
+                if (videoFile == null) {
+                  return;
+                }
+                ref.invalidate(postSettingProvider);
+                if (!context.mounted) {
+                  return;
+                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CreateNewPostView(
+                      fileToPost: videoFile,
+                      filetype: FileType.video,
+                    ),
+                  ),
+                );
+              },
               icon: const FaIcon(
                 FontAwesomeIcons.film,
               ),
             ),
             IconButton(
-              onPressed: () async {},
+              onPressed: () async {
+                final imageFile =
+                    await ImagePickerHelper.pickImageFromGallery();
+                if (imageFile == null) {
+                  return;
+                }
+                ref.invalidate(postSettingProvider);
+                if (!context.mounted) {
+                  return;
+                }
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => CreateNewPostView(
+                      fileToPost: imageFile,
+                      filetype: FileType.image,
+                    ),
+                  ),
+                );
+              },
               icon: const Icon(
                 Icons.add_photo_alternate_outlined,
               ),
